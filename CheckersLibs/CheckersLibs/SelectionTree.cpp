e56ca7f7dd46cd00dc1de394fb3d42c7092dbcd3
MOStLaurentCyr <mcyx03@gmail.com>
2015-12-07 13:17:25 -0500

    Begin Tree

--- CheckersLibs/CheckersLibs/SelectionTree.cpp
+++ CheckersLibs/CheckersLibs/SelectionTree.cpp
@@ -2,12 +2,52 @@
 #include "SelectionTree.h"
 
 
-SelectionTree::SelectionTree(Piece* tabCheckers[10][10])
+SelectionTree::SelectionTree(Move* lastMove)
 {
-	root = new Node(tabCheckers[10][10]);
+	_root = new Node(lastMove, nullptr);
 }
 
 
-SelectionTree::~SelectionTree(void)
+SelectionTree::~SelectionTree()
 {
+}
+
+void SelectionTree::add(Move* moveToAdd, SelectionTree::Node* parentNode)
+{
+	if(_root == nullptr)
+	{
+		_root = new Node(moveToAdd, nullptr);
+	}
+	else if(_root == parentNode)
+	{
+		bool nodeAdded = false;
+		for(int i = 0; i < _root->nbrOfChilds; i++)
+		{
+			if(_root->_tabChild[i] == nullptr)
+			{
+				_root->_tabChild[i] = new Node(moveToAdd, _root);
+				_root->nbrOfChilds++;
+				nodeAdded = true;
+			}
+		}
+		
+		if(nodeAdded == false)
+		{
+			Move* temp[40];
+		}
+	}
+	else
+	{
+		addBranch(moveToAdd, 0, parentNode, _root);
+	}
+}
+
+void SelectionTree::addBranch(Move* moveToAdd, int index, SelectionTree::Node* nodeToFind, SelectionTree::Node* currentNode)
+{
+
+}
+
+SelectionTree::Node::Node(Move* element, SelectionTree::Node* parent)
+{
+
 }
